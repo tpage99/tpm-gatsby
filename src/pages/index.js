16540b828189faad595343125fa78f72a2354e74
tpage99 <taylorvtx1300@gmail.com>
2019-01-06 15:02:47 -0600

    changed some paragraph text to test responsiveness

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,10 @@ export default class index extends Component {
     return (
       <div>
         <h1>This is the first of many tests</h1>
+        <p>
+          Just a bunch more text about how we're the Pages and we can do social
+          media and web development-type stuff.
+        </p>
       </div>
     );
   }
