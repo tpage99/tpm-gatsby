3c70fce9749bf9c9f89fd2c06fb54cf455db2bd6
tpage99 <taylorvtx1300@gmail.com>
2019-01-06 15:01:12 -0600

    first test changed text

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,11 @@
-import React from "react"
+import React, { Component } from 'react';
 
-export default () => <div>Hello world!</div>
+export default class index extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This is the first of many tests</h1>
+      </div>
+    );
+  }
+}
