909e7bbd200da45ba2c0a08e91a95f97c9110d55
tpage99 <taylorvtx1300@gmail.com>
2019-01-10 06:51:48 -0600

    added some styling and an about page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
+import Container from '../components/container';
 
 export default class index extends Component {
   render() {
     return (
-      <div>
-        <h1>This is the first of many tests</h1>
-        <p>
-          Just a bunch more text about how we're the Pages and we can do social
-          media and web development-type stuff.
-        </p>
-      </div>
+      <Container>
+        <div>
+          <h1>This is the first of many tests</h1>
+          <p>
+            Just a bunch more text about how we're the Pages and we can do
+            social media and web development-type stuff.
+          </p>
+        </div>
+      </Container>
     );
   }
 }
