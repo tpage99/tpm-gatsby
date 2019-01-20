5131d0631f42410e633f150aa0a25af4dc9c1631
tpage99 <taylorvtx1300@gmail.com>
2019-01-19 23:35:59 -0600

    added info to home page and updated with grid items

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,72 +1,69 @@
 import React from 'react';
 import Container from '../components/container';
-import Layout from '../components/layout';
 import { Link } from 'gatsby';
 import '../assets/main.css';
 
 export default () => (
   <Container>
-    <Layout>
-      <h1 style={{ textAlign: `center` }}>a little about us.</h1>
-      <div className="grid">
-        <div className="img-fluid">
-          <img
-            src="https://res.cloudinary.com/tpage99/image/upload/v1546835047/web%20assets/kendra_website_final.jpg"
-            alt="kendra portfolio"
-          />
-        </div>
-        <p className="text-block">
-          Kendra is the social media ninja who uses her skills to build brand
-          authentic language that helps companies speak to their customers like
-          a real person. What does that even mean? She is great at talking to
-          your customers in a way that genuinely represents your business and
-          makes them feel like they can talk back to you.
-        </p>
+    <h1 style={{ textAlign: `center` }}>a little about us.</h1>
+    <div className="grid">
+      <div className="img-fluid">
+        <img
+          src="https://res.cloudinary.com/tpage99/image/upload/v1546835047/web%20assets/kendra_website_final.jpg"
+          alt="kendra portfolio"
+        />
       </div>
-      <div className="grid">
-        <div className="img-fluid">
-          <img
-            src="https://res.cloudinary.com/tpage99/image/upload/v1546835049/web%20assets/taylor_website_final.jpg"
-            alt="taylor profile"
-          />
-        </div>
-        <p className="text-block start-text">
-          Taylor is the techie who loves learning about web development and
-          using technologies to solve real life problems. With a Masters degree
-          in counseling psychology and a lot of experience in social work as a
-          caseworker, Taylor uses technology regularly as a remedy and resource
-          for all sorts of issues.
-        </p>
+      <p className="text-block">
+        Kendra is the social media ninja who uses her skills to build brand
+        authentic language that helps companies speak to their customers like a
+        real person. What does that even mean? She is great at talking to your
+        customers in a way that genuinely represents your business and makes
+        them feel like they can talk back to you.
+      </p>
+    </div>
+    <div className="grid">
+      <div className="img-fluid">
+        <img
+          src="https://res.cloudinary.com/tpage99/image/upload/v1546835049/web%20assets/taylor_website_final.jpg"
+          alt="taylor profile"
+        />
       </div>
-      <h2>Why You Should Work With Us</h2>
-      <h5>We're not your typical "tech" people.</h5>
-      <p>
-        We won't talk to you about Javascript frameworks or [insert random tech
-        term here] and expect you to care or understand anything about it. We
-        went to school to learn how to talk, and more importantly, how to{' '}
-        <em>listen</em> to people.
+      <p className="text-block start-text">
+        Taylor is the techie who loves learning about web development and using
+        technologies to solve real life problems. With a Masters degree in
+        counseling psychology and a lot of experience in social work as a
+        caseworker, Taylor uses technology regularly as a remedy and resource
+        for all sorts of issues.
       </p>
-      <h5>You need to be online.</h5>
-      <p>
-        Maybe you've been doing ok with what you've got or without a website
-        until now but the truth is you're missing out on customers without some
-        sort of digital storefront. This gives customers an opportunity to learn
-        more about you at their own pace. You know you need a website, a
-        Facebook page, or maybe just a site revamp but you don't know or care
-        too much about the details - you just know you don't want to have to do
-        it yourself. Let us help!
-      </p>
-      <Link
-        to="/"
-        activeStyle={{
-          color: 'red',
-        }}
-        state={{
-          pleasant: 'reasonably',
-        }}
-      >
-        Home page
-      </Link>
-    </Layout>
+    </div>
+    <h2>Why You Should Work With Us</h2>
+    <h5>We're not your typical "tech" people.</h5>
+    <p>
+      We won't talk to you about Javascript frameworks or [insert random tech
+      term here] and expect you to care or understand anything about it. We went
+      to school to learn how to talk, and more importantly, how to{' '}
+      <em>listen</em> to people.
+    </p>
+    <h5>You need to be online.</h5>
+    <p>
+      Maybe you've been doing ok with what you've got or without a website until
+      now but the truth is you're missing out on customers without some sort of
+      digital storefront. This gives customers an opportunity to learn more
+      about you at their own pace. You know you need a website, a Facebook page,
+      or maybe just a site revamp but you don't know or care too much about the
+      details - you just know you don't want to have to do it yourself. Let us
+      help!
+    </p>
+    <Link
+      to="/"
+      activeStyle={{
+        color: 'red',
+      }}
+      state={{
+        pleasant: 'reasonably',
+      }}
+    >
+      Home page
+    </Link>
   </Container>
 );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,104 @@
 import React, { Component } from 'react';
 import Container from '../components/container';
 import { Link } from 'gatsby';
+import '../assets/main.css';
 
 export default class index extends Component {
   render() {
     return (
-      <Container>
+      <div>
+        <Container>
+          <div>
+            <h1>hi! we're the pages</h1>
+            <div className="grid">
+              <div className="img-fluid">
+                <img
+                  src="https://res.cloudinary.com/tpage99/image/upload/v1536888004/kendraNTaylor.png"
+                  alt="kendra and taylor"
+                />
+              </div>
+              <p className="text-block start-text">
+                We're a married couple living in the Springfield area that
+                enjoys helping companies we love become more accessible to
+                others. <br />
+                <br />
+                We provide social media and web based solutions that help put
+                local businesses within easy reach of people in the Springfield
+                area, even if they don't know you yet.
+              </p>
+            </div>
+          </div>
+        </Container>
         <div>
-          <h1>This is the first of many tests</h1>
-          <p>
-            Just a bunch more text about how we're the Pages and we can do
-            social media and web development-type stuff.
-          </p>
-          <Link
-            to="/about/"
-            activeStyle={{
-              color: 'red',
-            }}
-            state={{
-              pleasant: 'reasonably',
-            }}
-          >
-            About page
-          </Link>
+          <h2 style={{ textAlign: `center`, background: `rebeccapurple` }}>
+            check out some of the stuff we do.
+          </h2>
         </div>
-      </Container>
+        <Container>
+          <div className="grid">
+            <div className="img-fluid">
+              <img
+                src="https://res.cloudinary.com/tpage99/image/upload/v1546752856/web%20assets/KLSiteSkewedShadow.png"
+                alt="kevin lane's catering website snapshot"
+              />
+            </div>
+            <div className="text-front">
+              <h3>websites.</h3>
+              <p>
+                Websites are your digital storefront and a great way to make a
+                first impression with potential customers. We specialize in
+                static websites for local businesses. Low overhead, secure
+                websites that load very quickly.
+              </p>
+              <Link to="#">
+                <button>Learn More</button>
+              </Link>
+            </div>
+          </div>
+          <div className="grid">
+            <div className="img-fluid">
+              <img
+                src="https://res.cloudinary.com/tpage99/image/upload/v1546752861/web%20assets/photo_layers_3.png"
+                alt="examples of created content/media"
+              />
+            </div>
+            <div className="start-text">
+              <h3>content creation.</h3>
+              <p className="text-block">
+                Do you need to communicate some boring or routine information
+                but want to do it in a way that is appealing and encourages
+                interaction with customers? Because we do the whole pictures and
+                text thing to make your potluck at least look like it's going to
+                be really cool.
+              </p>
+              <Link to="#">
+                <button>Learn More</button>
+              </Link>
+            </div>
+          </div>
+          <div className="grid">
+            <div className="img-fluid">
+              <img
+                src="https://res.cloudinary.com/tpage99/image/upload/v1546752858/web%20assets/FBSiteSkewedShadow.png"
+                alt="cash saver 417 facebook page"
+              />
+            </div>
+            <div className="text-front">
+              <h3>social media.</h3>
+              <p>
+                We can help you set up and establish a social media presence.
+                It's not just about having a personal Facebook page but rather
+                having a specially crafted strategy that's geared towards
+                engagement.
+              </p>
+              <Link to="#">
+                <button>Learn More</button>
+              </Link>
+            </div>
+          </div>
+        </Container>
+        <Link to="/about/">About page</Link>
+      </div>
     );
   }
 }
