37229ccc8e778cee2e7edc3491f493909e03e6d2
tpage99 <taylorvtx1300@gmail.com>
2019-02-10 21:33:08 -0600

    added pwa functionality to test out, including image for icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,5 +25,21 @@ module.exports = {
       },
     },
     'gatsby-transformer-remark',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `thePagesMedia`,
+        short_name: `thePagesMedia`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: `standalone`,
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+        include_favicon: true, // Include favicon
+      },
+    },
+    'gatsby-plugin-offline',
   ],
 };
