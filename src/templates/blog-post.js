688e6a0e3c58c5ae78cb749565a611c36139e756
tpage99 <taylorvtx1300@gmail.com>
2019-03-23 10:37:47 -0500

    adding SOME metadata to blogposts just to ensure something is there

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import Navbar from '../components/Navbar/Navbar';
 import HomeIcon from '../components/HomeIcon/HomeIcon';
 import Appnav from '../components/Navbar/AppNav';
@@ -11,6 +12,21 @@ export default function Template({ data }) {
   //above is same as using const post = data.markdownRemark;
   return (
     <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Blog | thePagesMedia</title>
+        <link rel="canonical" href="https://www.thepagesmedia.com/blog" />
+        <meta
+          name="description"
+          content="A collection of writings designed to help our readers learn more about us, our services, and the people we work with."
+        />
+        <html lang="en" />
+        <meta name="docsearch:version" content="2.0" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,shrink-to-fit=no,viewport-fit=cover"
+        />
+      </Helmet>
       <Navbar />
       <HomeIcon />
       <main>
