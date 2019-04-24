d5b9e29becb35f3bba82038edc5fe38afdb8f02d
Taylor Page <taylor@thepagesmedia.com>
2019-04-24 12:08:23 -0500

    re-sorted some styling for the blog posts as presented on desktop view

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,25 +1,22 @@
-import React from "react";
-import { Helmet } from "react-helmet";
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import { graphql } from "gatsby";
 import Navbar from "../components/Navbar/Navbar";
 import HomeIcon from "../components/HomeIcon/HomeIcon";
 import Appnav from "../components/Navbar/AppNav";
 import BlogContainer from "../components/BlogContainer/BlogContainer";
-import { graphql } from "gatsby";
-import "../assets/main.css";
+import '../assets/main.css';
 
 export default function Template({ data }) {
   const { markdownRemark: post } = data;
-  //above is same as using const post = data.markdownRemark;
+  // above is same as using const post = data.markdownRemark;
   return (
     <div>
       <Helmet>
         <html lang="en" />
         <link rel="canonical" href={post.frontmatter.path} />
         <meta name="docsearch:version" content="2.0" />
-        <meta
-          name="viewport"
-          content="width=device-width,initial-scale=1,shrink-to-fit=no,viewport-fit=cover"
-        />
+        <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no,viewport-fit=cover" />
         <meta name="description" content={post.frontmatter.description} />
 
         <meta property="og:url" content={post.frontmatter.path} />
@@ -36,30 +33,21 @@ export default function Template({ data }) {
 
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:site" content="@taylorpage99" />
-        <meta
-          name="twitter:description"
-          content={post.frontmatter.description}
-        />
+        <meta name="twitter:description" content={post.frontmatter.description} />
         <meta name="twitter:title" content={post.frontmatter.title} />
         <meta
           name="twitter:image"
           content="https://res.cloudinary.com/tpage99/image/upload/v1550803731/web%20assets/highfive2.jpg"
         />
 
-        <meta
-          name="google-site-verification"
-          content="1R4rFQpnixwUVi2dmzSl37vwdeyFFoC69PRMC5H5edY"
-        />
+        <meta name="google-site-verification" content="1R4rFQpnixwUVi2dmzSl37vwdeyFFoC69PRMC5H5edY" />
       </Helmet>
       <Navbar />
       <HomeIcon />
       <main>
         <BlogContainer>
           <h1>{post.frontmatter.title}</h1>
-          <div
-            dangerouslySetInnerHTML={{ __html: post.html }}
-            className="simple-link blog-link"
-          />
+          <div dangerouslySetInnerHTML={{ __html: post.html }} className="simple-link blog-link" />
         </BlogContainer>
       </main>
       <Appnav />
