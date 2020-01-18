fd91dc5c73db6ed5a7f1d3f981f78c0ee0f2b5f3
Taylor Page <taylor@thepagesmedia.com>
2020-01-18 00:07:53 -0600

    some stuff done and working on setting up grid for layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
-import React, { Component } from "react";
-import { Link } from "gatsby";
+import React, { Component } from 'react';
+import { Link } from 'gatsby';
 
-import Container from "../components/container";
-import Appnav from "../components/Navbar/AppNav";
-import SiteMetadata from "../components/Site-Metadata";
-import "../assets/main.css";
+import Container from '../components/container';
+import Appnav from '../components/Navbar/AppNav';
+import SiteMetadata from '../components/Site-Metadata';
+import '../assets/main.css';
 
 export default class index extends Component {
   render() {
@@ -20,22 +20,55 @@ export default class index extends Component {
                 backgroundImage: `url(https://res.cloudinary.com/tpage99/image/upload/v1550803731/web%20assets/highfive2.jpg)`,
                 backgroundPosition: `center center`,
                 backgroundRepeat: `no-repeat`,
-                objectFit: `cover`
+                objectFit: `cover`,
+                textAlign: `center`,
               }}
             >
-              <h1>the pages media</h1>
+              <h1
+                style={{
+                  paddingTop: `20rem`,
+                  fontSize: `4.5rem`,
+                  color: `white`,
+                  textShadow: `rgba(0, 0, 0, 0.67) 5px 5px 8px`,
+                  textAlign: `center`,
+                }}
+              >
+                the pages media
+              </h1>
             </div>
-            <section>
-              <div>
+            <section style={{ textAlign: `center`, margin: `2rem 0rem` }}>
+              <p>
+                We offer <span style={{ fontStyle: `italic`, fontWeight: `bold` }}>digital solutions</span> for local
+                businesses to solve their biggest problems - like meeting their customers where they are. We work to
+                help you cut costs while increasing your exposure in our local 417 market.
+              </p>
+              <div
+                style={{
+                  backgroundColor: `orange`,
+                  padding: `2rem 1rem 0.5rem`,
+                  boxShadow: `rgba(0, 0, 0, 0.45) 2px 5px 5px`,
+                }}
+              >
+                <h2 style={{ marginBottom: `0`, fontWeight: `bold`, fontSize: `2rem` }}>dig·i·tal so·lu·tions</h2>
+                <p style={{ marginTop: `0`, fontStyle: `italic`, color: `gray` }}>/ˈdijidl/ /səˈlo͞oSH(ə)n/</p>
+                <p>
+                  Virtual or electronic products that help solve real world problems in a consistent, economic, and
+                  reliable manner. Websites, social media, content strategy, search engine optimization (SEO), business
+                  software as a service, and progressive web apps are all examples of digital solutions.
+                </p>
+              </div>
+            </section>
+            <section style={{ display: `grid`, gridTemplateColumns: `1fr 1fr 1fr 1fr` }}>
+              <div style={{ gridColumn: `span 2` }}>
                 <h2>services</h2>
               </div>
-              <div>
+              <div style={{ gridColumn: `span 2` }}>
                 <h2>about</h2>
               </div>
-              <div>
+              <div style={{ gridColumn: `span 2` }}>
                 <h2>blog</h2>
               </div>
-              <div>
+              <div style={{ gridColumn: `span 2` }}>
                 <h2>contact</h2>
               </div>
             </section>
